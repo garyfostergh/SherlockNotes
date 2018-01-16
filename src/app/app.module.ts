89a99cd3ac130d6d92ca48750c0d6f3ab39a3572
garyfostergh <garyfoster@cox.net>
2018-01-16 18:02:50 -0500

    added header and home components and moved nav to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFireDatabaseModule } from "angularfire2/database";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+const firebaseConfig = {
+  "apiKey": "AIzaSyBR-rZYNPBpBFIgMrQF7flr_lSnKwRytaU",
+  "databaseURL": "https://sherlocknotescase.firebaseio.com",
+  "storageBucket": "sherlocknotescase.appspot.com",
+  "authDomain": "sherlocknotescase.firebaseapp.com",
+  "messagingSenderId": "341997958663",
+  "projectId": "sherlocknotescase"
+  };
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
