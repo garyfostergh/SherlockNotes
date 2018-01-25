f9f0301028afa97d9a0b270089be45c98d6dcab6
tolgabalci <tolga.balci@gmail.com>
2018-01-25 17:20:09 -0500

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { AngularFireModule } from "angularfire2";
 import { AngularFireAuthModule } from "angularfire2/auth";
 import { AngularFireDatabaseModule } from "angularfire2/database";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { TopicsComponent } from './topics/topics.component';
-import { NotesComponent } from './notes/notes.component';
-import { NotebooksComponent } from './notebooks/notebooks.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+import { CategoriesComponent } from "./categories/categories.component";
+import { TopicsComponent } from "./topics/topics.component";
+import { NotesComponent } from "./notes/notes.component";
+import { NotebooksComponent } from "./notebooks/notebooks.component";
+import { RouterModule, Route } from "@angular/router";
 
 const firebaseConfig = {
-  "apiKey": "AIzaSyBR-rZYNPBpBFIgMrQF7flr_lSnKwRytaU",
-  "databaseURL": "https://sherlocknotescase.firebaseio.com",
-  "storageBucket": "sherlocknotescase.appspot.com",
-  "authDomain": "sherlocknotescase.firebaseapp.com",
-  "messagingSenderId": "341997958663",
-  "projectId": "sherlocknotescase"
-  };
+  apiKey: "AIzaSyBR-rZYNPBpBFIgMrQF7flr_lSnKwRytaU",
+  databaseURL: "https://sherlocknotescase.firebaseio.com",
+  storageBucket: "sherlocknotescase.appspot.com",
+  authDomain: "sherlocknotescase.firebaseapp.com",
+  messagingSenderId: "341997958663",
+  projectId: "sherlocknotescase"
+};
 
+const routes: Route[] = [
+  { path: "home", component: HomeComponent },
+  { path: "categories", component: CategoriesComponent },
+  { path: "notebooks", component: NotebooksComponent },
+  { path: "topics", component: TopicsComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "**", redirectTo: "home" }
+];
 
 @NgModule({
   declarations: [
@@ -36,10 +45,10 @@ const firebaseConfig = {
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
-    AngularFireDatabaseModule
-  
+    AngularFireDatabaseModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
